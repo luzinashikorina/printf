d4ca55ff4d2d261f4669be0011bc5033afdf5ab6
luzinashikorina <luzabiniam01@gmail.com>
2022-11-22 17:44:29 +0300

    l

--- itoa.c
+++ itoa.c
@@ -19,18 +19,22 @@ void reverse(char s[])
  * @n: the int
  * @s: the buffer of the receiving str
  * Return: nothing
- *i/
+ */
 void itoa(int n, char s[])
- {
-     int i, sign;
+{
+	int i = 0;
+	int sign = 0;
 
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
+	if (n < 0)
+	{
+		sign = -1;	
+		n *= -1;
+	}	 
+	do {
+		s[i++] = n % 10 + '0';  
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
